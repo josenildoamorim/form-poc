871c8f1c7bf692e98836b6e636e09e0c5d0fb7be
Josenildo Santos <josenildo.santos@concrete.com.br>
2018-06-02 13:21:43 -0300

    refatorando metodos, incluindo comentários para avaliação

--- features/page_objects/formulario.page.js
+++ features/page_objects/formulario.page.js
@@ -5,7 +5,10 @@ const EC = require('protractor').ExpectedConditions
 class Form {
   constructor () {
     this.body = $('body')
-    this.startBtn = element.all(by.xpath("//*[contains(text(),'start')]")).get(2)
+    // elemento do botão start que funciona no web, mas não no mobile.
+    this.startBtn = element(by.xpath('//*[@id="2733134"]/div[1]/div/div[3]/div[1]'))
+    // elemento do botão start que funciona no mobile, mas não no web.
+    // this.startBtn = element.all(by.xpath("//*[contains(text(),'start')]")).get(2)
     this.nameField = $$('input').get(0)
     this.lastNameField = $$('input').get(1)
     this.emailField = $$('input').get(2)
@@ -14,12 +17,21 @@ class Form {
     this.sucessMessage = element(by.cssContainingText('.text', 'Thanks for completing this typeform'))
   }
 
-  startForm () {
+  openPage () {
     browser.waitForAngularEnabled(false)
-    browser.get('https://stonepayments.typeform.com/to/yl5PKW')
-    this.waitForElement(this.startBtn)
-    // this.body.sendKeys(protractor.Key.ENTER)
+    return browser.get('https://stonepayments.typeform.com/to/yl5PKW')
+  }
+
+  startForm () {
+    // load me impede de clicar e não consegui mapear 
+    // o load para mandar o protractor aguardar por ele, logo usei sleep
+    // sleep alto devido a demora no mobile. não consegui fazer o wait funcionar
+    this.sleep(10000)
     return this.startBtn.click()
+
+    // para iniciar no browser, foi complicado clicar no botão. apenas por xpath,
+    // o que divergia do elemento no mobile e não podia mandar enter no mobile
+    // return this.body.sendKeys(protractor.Key.ENTER)
   }
 
   typeName (name) {
@@ -43,18 +55,22 @@ class Form {
   }
 
   selectCountry (country) {
-    browser.sleep(1000)
+    this.sleep(2000)
     this.countrySelector.sendKeys(country)
     return this.countrySelector.sendKeys(protractor.Key.ENTER)
   }
 
   clickRegister () {
-    browser.sleep(3000)
+    this.sleep(2000)
     return this.body.sendKeys(protractor.Key.ENTER)
   }
 
   waitForElement (element) {
     return browser.wait(EC.visibilityOf(element), 10000)
+  }
+
+  sleep (time) {
+    return browser.sleep(time)
   }
 }
 
